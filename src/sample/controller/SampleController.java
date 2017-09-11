48ccc30a7bd79c2a72a3a15c8dd63c74c056d4c3
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-09-11 08:24:29 -0600

    Added my PlayDohCircle.

--- src/sample/controller/SampleController.java
+++ src/sample/controller/SampleController.java
@@ -1,11 +1,23 @@
 package sample.controller;
 
+import java.util.Scanner;
+
 public class SampleController 
 {
-	
 	public void start()
 	{
 		System.out.println("This is a sample proect to show the github and");
-		System.out.println("eclipse process so I decelop muscle memory :D");
+		System.out.println("eclipse process so I develop muscle memory :D");
+	
+		testScanner();
+	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner; //Decres a Scanner object.
+		firstScanner = new Scanner(System.in); //Instanciates a Scanner object.
+		System.out.println("Please type your favouite food");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh, you like to eat " + answer);
 	}
 }
---
+++ src/sample/model/PlayDohCircle.java
@@ -0,0 +1,14 @@
+package sample.model;
+
+public class PlayDohCircle 
+{
+	//Declaration section
+	//Data member section
+	//NEED TO BE PRIVATE
+	private int size;
+	
+	public PlayDohCicle()
+	{
+		this.size = 5;
+	}
+}
